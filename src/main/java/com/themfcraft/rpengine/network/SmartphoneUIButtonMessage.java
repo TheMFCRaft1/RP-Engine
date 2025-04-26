91f6e3c9582cf7d1c9027a5660c496490de99295
TheMFCraft1 <2010.maxfriedrich@gmail.com>
2025-04-27 00:29:39 +0200

    bugfix

---
+++ src/main/java/com/themfcraft/rpengine/network/SmartphoneUIButtonMessage.java
@@ -0,0 +1,74 @@
+package com.themfcraft.rpengine.network;
+
+import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.function.Supplier;
+import java.util.HashMap;
+
+import com.themfcraft.rpengine.world.inventory.SmartphoneUIMenu;
+import com.themfcraft.rpengine.procedures.OpenSmartphoneUIEmergencyProcedure;
+import com.themfcraft.rpengine.RpEngineMod;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class SmartphoneUIButtonMessage {
+	private final int buttonID, x, y, z;
+
+	public SmartphoneUIButtonMessage(PacketBuffer buffer) {
+		this.buttonID = buffer.readInt();
+		this.x = buffer.readInt();
+		this.y = buffer.readInt();
+		this.z = buffer.readInt();
+	}
+
+	public SmartphoneUIButtonMessage(int buttonID, int x, int y, int z) {
+		this.buttonID = buttonID;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public static void buffer(SmartphoneUIButtonMessage message, PacketBuffer buffer) {
+		buffer.writeInt(message.buttonID);
+		buffer.writeInt(message.x);
+		buffer.writeInt(message.y);
+		buffer.writeInt(message.z);
+	}
+
+	public static void handler(SmartphoneUIButtonMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+		NetworkEvent.Context context = contextSupplier.get();
+		context.enqueueWork(() -> {
+			PlayerEntity entity = context.getSender();
+			int buttonID = message.buttonID;
+			int x = message.x;
+			int y = message.y;
+			int z = message.z;
+			handleButtonAction(entity, buttonID, x, y, z);
+		});
+		context.setPacketHandled(true);
+	}
+
+	public static void handleButtonAction(PlayerEntity entity, int buttonID, int x, int y, int z) {
+		World world = entity.world;
+		HashMap guistate = SmartphoneUIMenu.guistate;
+		// security measure to prevent arbitrary chunk generation
+		if (!world.isBlockLoaded(new BlockPos(x, y, z)))
+			return;
+		if (buttonID == 1) {
+
+			OpenSmartphoneUIEmergencyProcedure.execute(world, x, y, z, entity);
+		}
+	}
+
+	@SubscribeEvent
+	public static void registerMessage(FMLCommonSetupEvent event) {
+		RpEngineMod.addNetworkMessage(SmartphoneUIButtonMessage.class, SmartphoneUIButtonMessage::buffer, SmartphoneUIButtonMessage::new, SmartphoneUIButtonMessage::handler);
+	}
+}
